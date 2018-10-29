94c3bb6aba1c888bd784ee779ece1a5c04c2325c
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-29 10:08:31 +0100

    App sections

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+// Settings
 app.set('port', 3000);
 const port = app.get('port');
 
+// Middlewares
+
+// Routes
+
+// Static files
+
+// Listening the server
 app.listen(
     port,
     () => {
