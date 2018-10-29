8da7c0afb755f6dfb27e35e792d54ba21f90f273
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-29 11:08:24 +0100

    Added contact page and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,16 @@ const router = express.Router();
 router.get(
     '/',
     (req, res) => {
-        console.log('Path hit');
+        console.log('Path root hit');
         res.render('index', {'title': 'Demo Node Website'});
+    }
+);
+
+router.get(
+    '/contact',
+    (req, res) => {
+        console.log('Path contact hit');
+        res.render('contact', {'title': 'Contact Page'});
     }
 );
 
