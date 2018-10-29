ecfe32d8e1e4a802395440ed230341c030f8a6ec
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-29 10:06:36 +0100

    Basic app with express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Server works");
+const express = require('express');
+const app = express();
+
+app.set('port', 3000);
+const port = app.get('port');
+
+app.listen(
+    port,
+    () => {
+        console.log("Server listening on port " + port);
+    }
+);
